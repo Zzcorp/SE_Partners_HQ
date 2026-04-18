0da09256a33d4fbacd7ff4eff51b564457b24afc
Zzcorp <mathey.s.w@gmail.com>
2026-04-18 20:45:20 +0700

    Clear repository contents

--- hq/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class HqConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "hq"
-    verbose_name = "S&E Partners HQ"
--- hq/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from hq.consumers import EventsConsumer
-
-websocket_urlpatterns = [
-    path("ws/events", EventsConsumer.as_asgi()),
-]
